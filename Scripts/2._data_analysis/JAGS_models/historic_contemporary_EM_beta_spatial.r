6a44efb3a58cd2deabd0944fa0df337d3b8fa588
colin.averill <colin.averill@gmail.com>
2019-01-15 10:26:39 -0500

    added covariaite, further cranked up burnin on beta spatial model to 20k.

--- Scripts/2._data_analysis/JAGS_models/historic_contemporary_EM_beta_spatial.r
+++ Scripts/2._data_analysis/JAGS_models/historic_contemporary_EM_beta_spatial.r
@@ -12,18 +12,14 @@ output.path <- historic_contemporary_EM_spatial.path
 d <- readRDS(historic_contemporary_merge.path) #here drop the path to data on your computer.
 
 #subset for testing
-d <- d[1:200,]
-
-#create a distance matrix from lat/lon. for spatial covariance.
-points <- as.matrix(d[,c('longitude','latitude')])
-d.mat <- geosphere::distm(points) / 1000 #convert meters to km, since sometimes JAGS doesn't like big numbers.
+d <- d[1:500,]
 
 #get climate delta.
 d$mat.delta <- d$mat30 - d$tair.yr.set
 d$map.delta <- d$map30 - d$precip.yr.set
 
 #get x and y values.
-d <- d[,.(c.relEM,h.relEM,mat30,map30,mat.delta,map.delta,n.dep)]
+d <- d[,.(c.relEM,h.relEM,mat30,map30,mat.delta,map.delta,n.dep,longitude,latitude)]
 d <- d[complete.cases(d),]
 y <- d$c.relEM
 x <- d[,c('h.relEM','mat30','map30','mat.delta','map.delta','n.dep')]
@@ -31,13 +27,16 @@ x <- d[,c('h.relEM','mat30','map30','mat.delta','map.delta','n.dep')]
 x$intercept <- rep(1, nrow(x))
 x$hist_ndep <- x$h.relEM * x$n.dep
 
+#create a distance matrix from lat/lon. for spatial covariance.
+points <- as.matrix(d[,c('longitude','latitude')])
+d.mat <- geosphere::distm(points) / 1000 #convert meters to km, since sometimes JAGS doesn't like big numbers.
+
 #subset x for testing.
 x <- data.frame(x[,c('intercept','h.relEM')])
 
 #create JAGS data object.
-#jd <- list(y=y, x = as.matrix(x), N = nrow(x), N.pred = ncol(x))
 jd <- list(y=y, x = as.matrix(x), N = nrow(x), N.pred = ncol(x), 
-           D = d.mat, min.prior.range = min(d.mat) + 0.1, max.prior.range = max(d.mat))
+           D = d.mat, min.prior.range = min(d.mat) + 0.001, max.prior.range = max(d.mat), w.mu = rep(0, nrow(x)))
 
 #specify JAGS model.----
 jags.model = "
@@ -60,11 +59,10 @@ model{
     }
   }
   Prec.mat <- inverse(Cov.mat)
-  spatial_re ~ dmnorm(w.mu, Prec.mat) #w.mu defined in model loop (confusing, right?)
+  spatial_re ~ dmnorm(w.mu, Prec.mat) #w.mu defined in data. Just a vector of zeros.
   
   #beta regression model.
   for (i in 1:N){
-        w.mu[i]  <- 0 #this is for spatial random effect. Could be somewhere else?
            y[i]  ~  dbeta(p[i], q[i])
            p[i]  <- mu[i] * tau
            q[i]  <- (1 - mu[i]) * tau
@@ -78,10 +76,10 @@ jags.out <- run.jags(jags.model,
                      data=jd,
                      n.chains=3,
                      adapt = 500,
-                     burnin = 3000,
+                     burnin = 20000,
                      sample = 2000,
                      monitor=c('m','sigma.sq','phi'),
-                     method = 'rjparallel')
+                     method = 'rjags')
 
 #Save output.----
 saveRDS(jags.out, output.path)
