2f4125425bc77cf5cbac91832b5b0d4762732160
colin.averill <colin.averill@gmail.com>
2019-06-06 06:33:24 -0400

    Updated forest.sim.r to run in parallel, deleted test version, forest.sim2.r

--- project_functions/forest.sim.r
+++ project_functions/forest.sim.r
@@ -1,8 +1,20 @@
-
 forest.sim <- function(g.mod, r.mod.am, r.mod.em, m.mod, 
                        initial_density = 20, n.plots = 100, n.step = 20, 
-                       env.cov = NA,
+                       env.cov = NA, n.cores = NA,
                        myco.split = 'within_plot'){
+  #Compatibility tests.----
+  #Check if doParallel is installed.
+  if('doParallel' %in% rownames(installed.packages()) == F){
+    stop('This function requires the doParallel package, please install it.\n')
+  }
+  #If doParallel is not loaded, load it.
+  if("doParallel" %in% (.packages()) == F){
+    library(doParallel)
+  }
+  #Check if rowr is installed.
+  if('rowr' %in% rownames(installed.packages()) == F){
+    stop('This function requires the rowr package, please install it.\n')
+  }
   #Check inputs are comaptible.
   if(initial_density %% 2 != 0){
     stop('Initial stem density needs to be an even, integer value.\n')
@@ -10,6 +22,12 @@ forest.sim <- function(g.mod, r.mod.am, r.mod.em, m.mod,
   if(n.plots         %% 2 != 0){
     stop('n.plots needs to be an even, integer value.\n')
   }
+  
+  #Register parallel environment.----
+  if(is.na(n.cores)){
+    n.cores <- detectCores()
+  }
+  registerDoParallel(n.cores)
   
   #build the initial tree and plot tables.----
   tree <- matrix(data = 12.7,nrow = initial_density, ncol = 1)
@@ -32,7 +50,7 @@ forest.sim <- function(g.mod, r.mod.am, r.mod.em, m.mod,
       if(i >  n.plots/2){plot.list[[i]] <- tree.2}
     }
   }
-
+  
   
   #get plot table with plot level characteristics.
   plot.table <- list()
@@ -57,46 +75,45 @@ forest.sim <- function(g.mod, r.mod.am, r.mod.em, m.mod,
   #Begin simulation!----
   for(t in 1:n.step){
     #1. Grow and kill your trees.----
-    for(j in 1:length(plot.list)){
-      #grab tree table for a given plot.
-      cov <- plot.list[[j]]
-      colnames(cov)[1] <- c('PREVDIA.cm')
-      #merge plot-level covariates into tree table
-      cov <- rowr::cbind.fill(cov, plot.table[j,])
-      #grow your trees.
-      tree.new <- data.frame(predict(g.mod, newdata = cov))
-      #kill your trees.
-      tree.dead <- predict(m.mod, newdata = cov)
-      tree.dead <- rbinom(length(tree.dead),1, boot::inv.logit(tree.dead))
-      tree.new  <- data.frame(tree.new[!(tree.dead == 1),])
-      tree.new$em  <- cov$em[!(tree.dead == 1)]
-      colnames(tree.new) <- c('DIA.cm','em')
-      #update your tree table.
-      plot.list[[j]] <- tree.new
-    } #end individuals within plots loop.
-    
-    #2. recruit new trees!----
-    recruits.prob.am <- exp(predict(r.mod.am, newdata = plot.table))
-    recruits.prob.em <- exp(predict(r.mod.em, newdata = plot.table))
-    recruits.am      <- rpois(length(recruits.prob.am), recruits.prob.am)
-    recruits.em      <- rpois(length(recruits.prob.em), recruits.prob.em)
-    for(i in 1:length(recruits.am)){
-      if(recruits.am[i] == 0 & recruits.em[i] == 0){next}
-      #Recruit AM trees.
-      new.recruit.am <- matrix(data = 12.7,nrow = recruits.am[i], ncol = 1)
-      em             <- matrix(data =    0,nrow = recruits.am[i], ncol = 1)
-      colnames(new.recruit.am) <- 'DIA.cm'
-      colnames(em)             <- 'em'
-      new.recruit.am <- cbind(new.recruit.am, em)
-      #Recruit EM trees.
-      new.recruit.em <- matrix(data = 12.7,nrow = recruits.em[i], ncol = 1)
-      em             <- matrix(data =    1,nrow = recruits.em[i], ncol = 1)
-      colnames(new.recruit.em) <- 'DIA.cm'
-      colnames(em)             <- 'em'
-      new.recruit.em <- cbind(new.recruit.em, em)
-      #merge new recruits into the tree table.
-      plot.list[[i]] <- rbind(plot.list[[i]], new.recruit.am, new.recruit.em)
-    }
+    new.plot.list <- list()
+    new.plot.list <- 
+      foreach(j = 1:length(plot.list)) %dopar% {
+        #grab tree table for a given plot.
+        cov <- plot.list[[j]]
+        colnames(cov)[1] <- c('PREVDIA.cm')
+        #merge plot-level covariates into tree table
+        cov <- rowr::cbind.fill(cov, plot.table[j,])
+        #grow your trees.
+        tree.new <- data.frame(predict(g.mod, newdata = cov))
+        #kill your trees.
+        tree.dead <- predict(m.mod, newdata = cov)
+        tree.dead <- rbinom(length(tree.dead),1, boot::inv.logit(tree.dead))
+        tree.new  <- data.frame(tree.new[!(tree.dead == 1),])
+        tree.new$em  <- cov$em[!(tree.dead == 1)]
+        colnames(tree.new) <- c('DIA.cm','em')
+        #recruit new trees.
+        recruits.prob.am <- exp(predict(r.mod.am, newdata = plot.table[j,]))
+        recruits.prob.em <- exp(predict(r.mod.em, newdata = plot.table[j,]))
+        recruits.am      <- rpois(length(recruits.prob.am), recruits.prob.am)
+        recruits.em      <- rpois(length(recruits.prob.em), recruits.prob.em)
+        #AM recruits.
+        new.recruit.am <- matrix(data = 12.7,nrow = recruits.am, ncol = 1)
+        em             <- matrix(data =    0,nrow = recruits.am, ncol = 1)
+        colnames(new.recruit.am) <- 'DIA.cm'
+        colnames(em)             <- 'em'
+        new.recruit.am <- cbind(new.recruit.am, em)
+        #EM recruits.
+        new.recruit.em <- matrix(data = 12.7,nrow = recruits.em, ncol = 1)
+        em             <- matrix(data =    1,nrow = recruits.em, ncol = 1)
+        colnames(new.recruit.em) <- 'DIA.cm'
+        colnames(em)             <- 'em'
+        new.recruit.em <- cbind(new.recruit.em, em)
+        
+        #update your tree table.
+        to_return <- rbind(tree.new, new.recruit.em, new.recruit.am)
+        return(to_return)
+      } #end parallel plot loop.
+    plot.list <- new.plot.list
     
     #3. Update plot table.----
     plot.table <- list()
